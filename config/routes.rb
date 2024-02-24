4a5a8aeffae500405ced40a325000a9f4ebe4be7
Joshua Mathews <joshfeltonm@gmail.com>
2024-02-24 14:42:11 -0700

    Add admins

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_scope :admin do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
